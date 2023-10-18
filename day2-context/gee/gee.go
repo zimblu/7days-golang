e041d5ad779022ead5f2aa9b55bdf96cb535ae83
zimblu_ <2387724492@qq.com>
2023-10-18 13:53:47 +0800

    Initial commit

---
+++ day2-context/gee/gee.go
@@ -0,0 +1,40 @@
+package gee
+
+import "net/http"
+
+// HandlerFunc defines the request handler used by gee
+type HandlerFunc func(*Context)
+
+// Engine implement the interface of ServeHTTP
+type Engine struct {
+	router *router
+}
+
+// New is the constructor of gee.Engine
+func New() *Engine {
+	return &Engine{router: newRouter()}
+}
+
+func (e *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
+	e.router.addRoute(method, pattern, handler)
+}
+
+// GET defines the method to add GET request
+func (e *Engine) GET(pattern string, handler HandlerFunc) {
+	e.addRoute("GET", pattern, handler)
+}
+
+// POST defines the method to add POST request
+func (e *Engine) POST(pattern string, handler HandlerFunc) {
+	e.addRoute("POST", pattern, handler)
+}
+
+// Run defines the method to start a http server
+func (e *Engine) Run(addr string) (err error) {
+	return http.ListenAndServe(addr, e)
+}
+
+func (e *Engine) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	c := newContext(w, r)
+	e.router.handle(c)
+}
